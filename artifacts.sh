1b15e3731a7c0cd9ed399e5f997b684c3d0dc271
mpl <mathieu.lonjaret@gmail.com>
2018-12-06 02:54:02 +0100

    s/stage/step/

--- artifacts.sh
+++ artifacts.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-echo stage0
+echo step0
 if [ ! -d /foo ]; then
 	mkdir /foo
 	exit 0
 fi
 
-echo stage1
+echo step1
 if [ ! -d /foo/bar ]; then
 	mkdir /foo/bar
 	exit 0
 fi
 
-echo stage2
+echo step2
 if [ ! -d /foo/bar/baz ]; then
 	mkdir /foo/bar/baz
 	exit 0
 fi
 
-echo stage3
+echo step3
 if [ ! -d /foo/bar/baz/yo ]; then
 	mkdir /foo/bar/baz/yo
 	exit 0
 fi
-
-echo stage4
 
