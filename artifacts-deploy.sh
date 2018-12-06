65e6c89c1cdac03c05564b172cf7d19a21e4cbce
mpl <mathieu.lonjaret@gmail.com>
2018-12-06 19:42:28 +0100

    deploy steps

---
+++ artifacts-deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Since it happens in the first run, /foo ends up being saved in the base image,
+# but nothing else after ever ends up being committed to an image.
+if [ ! -d /foo-d ]; then
+	mkdir /foo-d
+	echo step0-d
+	exit 0
+fi
+
+# So we start from here on every run that starts with the base image, i.e. every
+# run from the second included.
+if [ ! -d /foo-d/bar ]; then
+	mkdir /foo-d/bar
+	echo step1-d
+	exit 0
+fi
+
+# This step below should never happen.
+if [ ! -d /foo-d/bar/baz ]; then
+	mkdir /foo-d/bar/baz
+	echo step2-d
+	exit 0
+fi
