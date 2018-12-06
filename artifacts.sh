6157ab98632c9a83f4a309d9127dfa22a21f2f5f
mpl <mathieu.lonjaret@gmail.com>
2018-12-06 03:46:15 +0100

    only ouptput steps when actually doing them

--- artifacts.sh
+++ artifacts.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-echo step0
 if [ ! -d /foo ]; then
 	mkdir /foo
+	echo step0
 	exit 0
 fi
 
-echo step1
 if [ ! -d /foo/bar ]; then
 	mkdir /foo/bar
+	echo step1
 	exit 0
 fi
 
-echo step2
 if [ ! -d /foo/bar/baz ]; then
 	mkdir /foo/bar/baz
+	echo step2
 	exit 0
 fi
 
-echo step3
 if [ ! -d /foo/bar/baz/yo ]; then
 	mkdir /foo/bar/baz/yo
+	echo step3
 	exit 0
 fi
-
