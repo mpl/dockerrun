ec8657fb6d8f230d294598c337b655229fbb0bbf
mpl <mathieu.lonjaret@gmail.com>
2019-01-04 02:23:18 +0100

    sleep.sh: redirect background process output
    so that docker exec does not wait for it
    
    related: https://github.com/moby/moby/issues/33039

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM golang:latest
 
+RUN apt-get update
+RUN apt-get install -y vim
 RUN mkdir -p src/github.com/mpl
 WORKDIR src/github.com/mpl
 RUN git clone https://github.com/mpl/dockerrun.git
--- sleep.sh
+++ sleep.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-go run sleeper/main.go &
+# redirecting output so docker exec does not wait for it indefinitely.
+go run sleeper/main.go > /dev/null 2>&1 &
 echo "sleeper is in background"
-
